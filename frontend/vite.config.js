632b47d3e36d31c0509825cb988ed65ed145b69c
feather-tail <imeliaslake@gmail.com>
2025-03-06 18:18:54 +0300

    update deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,11 +6,9 @@ export default defineConfig({
   server: {
     port: 5002,
     proxy: {
-      // Проксируем запросы к API
       '/api': {
         target: 'http://localhost:5001',
       },
-      // Проксируем WebSocket соединения
       '/socket.io': {
         target: 'ws://localhost:5001',
         ws: true,
