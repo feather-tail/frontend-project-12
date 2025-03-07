40cdf9dea8681b334deff78658de3657b12415b1
Ilya Ryzhkov <imeliaslake@gmail.com>
2025-03-08 01:45:39 +0300

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { PageOne } from './404.jsx';
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { PageOne } from './Components/Pages';
 
 function App() {
   return (
