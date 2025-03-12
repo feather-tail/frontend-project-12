a34e1d547f2025c0bb62c193e866b637914dfedc
feather-tail <imeliaslake@gmail.com>
2025-03-12 18:06:49 +0300

    add authorization

--- frontend/src/PageLogin.jsx
+++ frontend/src/PageLogin.jsx
@@ -1,39 +1,63 @@
 import React from 'react';
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const LoginSchema = Yup.object().shape({
   username: Yup.string()
-    .min(6, 'Минимум 6 символов')
+    .min(5, 'Минимум 5 символов')
     .required('Обязательное поле'),
   password: Yup.string()
-    .min(6, 'Минимум 6 символов')
+    .min(5, 'Минимум 5 символов')
     .required('Обязательное поле'),
 });
 
 const LoginForm = () => {
+  const url = 'http://localhost:5001';
+  const navigate = useNavigate();
   return (
     <Formik
       initialValues={{ username: '', password: '' }}
       validationSchema={LoginSchema}
-      onSubmit={(values, { setSubmitting }) => {
-        console.log('Отправка формы:', values);
+      onSubmit={async (values, { setSubmitting }) => {
+        try {
+          const response = await axios.post(url, {
+            name: values.name,
+            password: values.password,
+          });
+          console.log(response.data);
+          localStorage.setItem('token', response.data.token);
+          navigate('/');
+        } catch (error) {
+          console.log('Ошибка авторизации:', error.response.data.message);
+          navigate('/Page401');
+        }
         setSubmitting(false);
       }}
     >
-      {({ errors, touched }) => (
+      {({ values, errors, touched }) => (
         <Form>
           <div>
             <label htmlFor='username'>Имя пользователя</label>
-            <Field id='username' name='username' type='text' />
+            <Field
+              id='username'
+              name='username'
+              type='text'
+              value={values.name}
+            />
             {errors.username && touched.username ? (
               <div>{errors.username}</div>
             ) : null}
           </div>
-
           <div>
             <label htmlFor='password'>Пароль</label>
-            <Field id='password' name='password' type='password' />
+            <Field
+              id='password'
+              name='password'
+              type='password'
+              value={values.password}
+            />
             {errors.password && touched.password ? (
               <div>{errors.password}</div>
             ) : null}
