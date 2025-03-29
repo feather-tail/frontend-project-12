08586543cc819180ac2cf929923532f13549ecd0
Ilya Ryzhkov <imeliaslake@gmail.com>
2025-03-30 01:17:06 +0300

    delete path svg

--- frontend/src/ChatPage.jsx
+++ frontend/src/ChatPage.jsx
@@ -71,8 +71,6 @@ const ChatPage = () => {
                 height='20'
                 fill='currentColor'
               >
-                <path d='M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z' />
-                <path d='M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4' />
               </svg>
               <span className='visually-hidden'>+</span>
             </button>
@@ -150,14 +148,6 @@ const ChatPage = () => {
                       height='20'
                       fill='currentColor'
                     >
-                      <path
-                        fillRule='evenodd'
-                        d='M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 
-                       1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 
-                       2 0 0 1-2 2H2a2 2 0 0 1-2-2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 
-                       2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.
-                       708L10.293 7.5z'
-                      />
                     </svg>
                     <span className='visually-hidden'>Отправить</span>
                   </button>
