999dce16ed8c2a11e8afc9f2bb268f3133d374e4
feathertail <imeliaslake@gmail.com>
2025-04-16 13:47:11 +0300

    fix linter

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -170,7 +170,10 @@ const ChatPage = () => {
             <div className="d-flex flex-column h-100">
               <div className="bg-light mb-4 p-3 shadow-sm small">
                 <p className="m-0">
-                  <b>#{currentChannel?.name}</b>
+                  <b>
+                    #
+                    {currentChannel?.name}
+                  </b>
                 </p>
                 <span className="text-muted">
                   {t('chat.messagesCounter', { count: messages.length })}
@@ -183,7 +186,11 @@ const ChatPage = () => {
               >
                 {messages.map((msg) => (
                   <div key={msg.id} className="text-break mb-2">
-                    <b>{msg.username || 'user'}:</b> {msg.body}
+                    <b>
+                      {msg.username || 'user'}
+                      :
+                    </b>
+                    {msg.body}
                   </div>
                 ))}
               </div>
